e76b42f00f936f268db5616ed08eb73076abb55f
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-11-24 14:28:36 -0500

    Initial Express setup, serving cached data

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let data = require('./data');
+
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get('/health', (req, res) =>
+{
+    res.json({success: true});
+})
+
+app.get('/data', (req, res) =>
+{
+    res.json(data);
+})
+
+app.listen(PORT, () => console.log('listening on port ' + PORT));
